25487963464ce6ed83b7d479bf7e882badd9d466
Kariakito <pilarmaestre@gmail.com>
2023-12-04 16:19:06 +0000

    Text color & size

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -5,16 +5,21 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration:  const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 176, 140, 240),
-                Color.fromARGB(255, 129, 16, 209)
-              ],
-            ),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 176, 140, 240),
+              Color.fromARGB(255, 106, 16, 209),
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
           ),
           child: const Center(
-            child: Text('Hola Flutter es Pilar!!!'),
+            child: Text(
+              'Hola Flutter es Pilar!!!',
+              style: TextStyle(
+                color: Colors.white24,
+                fontSize: 28,
+                fontStyle:FontStyle.normal
+              ),
+            ),
           ),
         ),
       ),
