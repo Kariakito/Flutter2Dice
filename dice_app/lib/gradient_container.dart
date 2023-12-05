0950346908a0b4b7b2c1b9a7fb75732b53afc8bb
Kariakito <pilarmaestre@gmail.com>
2023-12-05 12:35:06 +0000

    adding Variables to the Gradient Container file 2

--- dice_app/lib/gradient_container.dart
+++ dice_app/lib/gradient_container.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:dice_app/style_text.dart';
 
+var startAlignment = Alignment.topCenter;
+var endAlignment = Alignment.bottomCenter;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
-   Widget build(context) {
+  Widget build(context) {
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 176, 140, 240),
-              Color.fromARGB(255, 106, 16, 209),
-            ], 
-            begin: Alignment.topCenter, 
-            end: Alignment.bottomCenter
-            ),
-          ),
-          child: const Center(
-            child: StyleText(),
-          ),
-        ); 
-   }
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: const [
+            Color.fromARGB(255, 176, 140, 240),
+            Color.fromARGB(255, 106, 16, 209),
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyleText(),
+      ),
+    );
+  }
 }
-
