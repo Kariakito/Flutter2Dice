ecbd8c4d50a23ee58e4db8c882e8cff3a371c225
Kariakito <pilarmaestre@gmail.com>
2023-12-05 16:25:06 +0000

    dice change form N2 to N4 using setState

--- dice_app/lib/gradient_container.dart
+++ dice_app/lib/gradient_container.dart
@@ -1,5 +1,7 @@
+import 'package:dice_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 import 'package:dice_app/style_text.dart';
+import 'package:dice_app/dice_roller.dart';
 
 const startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
@@ -22,26 +24,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-images/dice-2.png',
-              width: 300,
-            ),
-            const SizedBox(height: 80,),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                  foregroundColor: Colors.white24,
-                  textStyle: const TextStyle(
-                    fontSize: 28,
-                  )),
-              child: const Text('Roll Dice/ Lanza el Dado'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
