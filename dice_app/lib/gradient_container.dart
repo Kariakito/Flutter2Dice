d495ff6028bea63f5688b5a6820020e7ae14b3ae
Kariakito <pilarmaestre@gmail.com>
2023-12-05 10:40:17 +0000

    adding Gradient Container file

---
+++ dice_app/lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+   Widget build(context) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 176, 140, 240),
+              Color.fromARGB(255, 106, 16, 209),
+            ], 
+            begin: Alignment.topCenter, 
+            end: Alignment.bottomCenter
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hola Flutter es Pilar!!!',
+              style: TextStyle(
+                color: Colors.white24,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ); 
+   }
+}
--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:dice_app/gradient_container.dart';
+
 void main() {
   runApp(
     const MaterialApp(
@@ -10,29 +12,3 @@ void main() {
   );
 }
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-   Widget build(context) {
-    return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 176, 140, 240),
-              Color.fromARGB(255, 106, 16, 209),
-            ], 
-            begin: Alignment.topCenter, 
-            end: Alignment.bottomCenter
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hola Flutter es Pilar!!!',
-              style: TextStyle(
-                color: Colors.white24,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ); 
-   }
-}
